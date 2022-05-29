9e8979afcb42bf2bffde7b0e71dcf52483001126
Prawoto Hadi <pawoto.hadi@gmail.com>
2022-05-29 21:00:20 +0700

    Done

--- index.js
+++ index.js
@@ -19,36 +19,59 @@ function lakukanLooping(arrPegawai) {
 
   /*
     TODO 1: Buatlah sebuah variabel bernama "hasilLooping" 
-      yang berisi gabungan nama depan dan belakang dari masing masing pegawai
-
-      Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
+    yang berisi gabungan nama depan dan belakang dari masing masing pegawai
+ 
+    Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
-
+   
+  let hasilLooping = [];
+  dataYangAkanDilooping.map(pegawai => {
+    hasilLooping.push(pegawai.namaDepan + ' ' + pegawai.namaBelakang);
+  });
+ 
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
-      yang berisi jumlah pria dari masing masing pegawai
+    yang berisi jumlah pria dari masing masing pegawai
   */
-  let jumlahPria = null;
-
+  let jumlahPria = 0;
+  dataYangAkanDilooping.map(pegawai => {
+    if (pegawai.jenisKelamin == "M") {
+      jumlahPria++;
+    }
+  });
+ 
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
-      yang berisi jumlah wanita dari masing masing pegawai
+    yang berisi jumlah wanita dari masing masing pegawai
   */
-  let jumlahWanita = null;
-
+  let jumlahWanita = 0;
+  dataYangAkanDilooping.map(pegawai => {
+    if (pegawai.jenisKelamin == "F") {
+      jumlahPria++;
+    }
+  });
+ 
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
-      yang akan mengomentari apakah lebih banyak Pria atau Wanita
+    yang akan mengomentari apakah lebih banyak Pria atau Wanita
       
-      Apabila Pria lebih banyak dari Wanita, maka komentar akan berisi:
-        "Jumlah Pria lebih banyak dari Wanita"
-      Apabila Wanita lebih banyak dari Pria, maka komentar akan berisi:
-        "Jumlah Wanita lebih banyak dari Pria"
-      Apabila imbang, maka komentar akan berisi:
-        "Jumlah Pria dan Wanita berimbang"
+    Apabila Pria lebih banyak dari Wanita, maka komentar akan berisi:
+      "Jumlah Pria lebih banyak dari Wanita"
+    Apabila Wanita lebih banyak dari Pria, maka komentar akan berisi:
+      "Jumlah Wanita lebih banyak dari Pria"
+    Apabila imbang, maka komentar akan berisi:
+      "Jumlah Pria dan Wanita berimbang"
   */
-  let komentar = null;
+  let komentar = '';
+  if (jumlahPria > jumlahWanita) {
+    komentar = 'Jumlah Pria lebih banyak dari Wanita'
+  } 
+  else if (jumlahPria < jumlahWanita) {
+    komentar = 'Jumlah Wanita lebih banyak dari Pria'
+  } 
+  else {
+    komentar = 'Jumlah Pria dan Wanita berimbang'
+  }
 
   // ! JANGAN DIMODIFIKASI
   return {
